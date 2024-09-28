7c552128f0e44201f8c79ff534bd478927291b88
liuj012 <145478562+liuj012@users.noreply.github.com>
2024-09-27 21:15:28 -0400

    test

---
+++ script.js
@@ -0,0 +1,60 @@
+let scrollSpeed = 1;
+let autoScrollInterval;
+let userScrollTimeout;
+
+const scrollContainer = document.querySelector('.scroll-container');
+
+function startAutoScroll() {
+    autoScrollInterval = setInterval(() => {
+        scrollContainer.scrollLeft += scrollSpeed;
+
+        if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth - 100) {
+            reorderRight();
+        }
+    }, 10);
+}
+
+function detectManualScroll() {
+    clearInterval(autoScrollInterval);
+
+    if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth - 100) {
+        reorderRight();
+    } else if (scrollContainer.scrollLeft <= 50) {
+        reorderLeft();
+    }
+
+    clearTimeout(userScrollTimeout);
+    userScrollTimeout = setTimeout(() => {
+        startAutoScroll();
+    }, 200);
+}
+
+function reorderRight() {
+    const firstItem = scrollContainer.firstElementChild;
+
+    const clone = firstItem.cloneNode(true);
+    scrollContainer.appendChild(clone);
+
+    scrollContainer.scrollLeft -= firstItem.offsetWidth;
+    
+    scrollContainer.removeChild(firstItem);
+}
+
+function reorderLeft() {
+    const lastItem = scrollContainer.lastElementChild;
+
+    const clone = lastItem.cloneNode(true);
+    scrollContainer.insertBefore(clone, scrollContainer.firstElementChild);
+
+
+    scrollContainer.scrollLeft += lastItem.offsetWidth;
+    
+    scrollContainer.removeChild(lastItem);
+}
+
+scrollContainer.addEventListener('wheel', detectManualScroll);
+scrollContainer.addEventListener('touchstart', detectManualScroll);
+scrollContainer.addEventListener('touchmove', detectManualScroll);
+
+startAutoScroll();
+
